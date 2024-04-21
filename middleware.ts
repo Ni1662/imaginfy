4d82a6b036096fd84fb56f21b0bcc1cb281b1388
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-21 11:07:16 +0530

    clerk setup

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoute = createRouteMatcher([
+  "/",
+  "/cedits",
+  "/transformations(.*)",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
