a1eb87fde1db0642680719f3334a6381e21571a0
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-27 10:46:17 +0530

    form

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -4,7 +4,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import { Button } from "@/components/ui/button";
+
 import {
   Form,
   FormControl,
@@ -14,10 +23,13 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { defaultValues } from "@/constants";
 
-const formSchema = z.object({
+import { Input } from "@/components/ui/input";
+import { defaultValues, transformationTypes } from "@/constants";
+import { CustomField } from "./CustomField";
+import { useState } from "react";
+
+export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   color: z.string().optional(),
@@ -28,7 +40,15 @@ const formSchema = z.object({
 const TransformationForm = ({
   action,
   data = null,
+  userId,
+  type,
+  creditBalance,
 }: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+
+  const [image, setImage] = useState(data)
+  const [newTransformation, setNewTransformation] = useState<Transformations | null>(null)
+
   const initialValues =
     data && action === "Update"
       ? {
@@ -48,30 +68,36 @@ const TransformationForm = ({
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    
     console.log(values);
   }
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name="title"
+          formLabel="Image Title"
+          className=" w-full"
+          render={({ field }) => <Input {...field} className="input-field" />}
         />
-        <Button type="submit">Submit</Button>
+
+        {type === "fill" && (
+          <CustomField
+            render={({ field }) => (
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
       </form>
     </Form>
   );
