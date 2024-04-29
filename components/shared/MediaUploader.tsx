0ede4ed0e638c5e3d90c1e9ec54baf25e4ce13e8
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-29 19:34:42 +0530

    cloudinary setup

---
+++ components/shared/MediaUploader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useToast } from "@/components/ui/use-toast"
+
+
+const MediaUploader = () => {
+
+    const { toast } = useToast()
+
+  return (
+    <div>MediaUploader</div>
+  )
+}
+
+export default MediaUploader
