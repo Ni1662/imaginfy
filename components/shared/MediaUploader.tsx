10b3c9bc2ad2a54ff852eeb396d2a5bc711a265f
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-04-30 22:46:07 +0530

    media uploader

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,14 +1,59 @@
-import React from 'react'
-import { useToast } from "@/components/ui/use-toast"
+import React from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { CldUploadWidget } from "next-cloudinary";
 
+type MediaUploaderProps = {
+  onValueChange: (value: string) => void;
+  setImage: React.Dispatch<any>;
+  publicId: string;
+  image: any;
+  type: string;
+};
 
-const MediaUploader = () => {
+const MediaUploader = ({
+  onValueChange,
+  setImage,
+  image,
+  publicId,
+  type,
+}: MediaUploaderProps) => {
+  const { toast } = useToast();
 
-    const { toast } = useToast()
+  const onUploadSuccessHandler = (result: any) => {
+    toast({
+      title: "Image uploaded successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: "success-toast",
+    });
+  };
+
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "Something went wrong while uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast",
+    });
+  };
 
   return (
-    <div>MediaUploader</div>
-  )
-}
+    <CldUploadWidget
+      uploadPreset="imaginify"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    >
+      {({ open }) => (
+        <div className="flex flex-col gap-4">
+          <h3 className="h3-bold text-dark-600">Original</h3>
+        </div>
+      )}
+    </CldUploadWidget>
+  );
+};
 
-export default MediaUploader
+export default MediaUploader;
