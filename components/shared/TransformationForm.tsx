51aace848981ccda4f52f48204d2fc3788b80ae5
Nitesh Agarwal <niteshkumar.nk649@gmail.com>
2024-05-01 13:29:10 +0530

    transfoem image

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -35,6 +35,7 @@ import { useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -235,6 +236,14 @@ const TransformationForm = ({
                 type={type}
               />
             )}
+          />
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
           />
         </div>
 
